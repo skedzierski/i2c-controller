929a4ae48fa389716c76cc029d40e971e0b544b9
Szymon Kędzierski <szymon.kedzierski2.stud@pw.edu.pl>
2024-03-13 20:22:38 +0100

    implement i2c clock

---
+++ VIVADO/PUF_24L_I2C.srcs/sources_1/imports/RTL/counter.vhd
@@ -0,0 +1,37 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use IEEE.numeric_std.all; 
+
+entity generic_counter is
+generic(
+    counter_width: natural
+);
+
+port(
+    clk: in std_logic;
+    rst: in std_logic;
+    preload: in std_logic_vector(counter_width-1 downto 0);
+    o_cnt: out std_logic
+);
+
+end;
+
+architecture rtl of generic_counter is
+    signal current_val : std_logic_vector(counter_width-1 downto 0);
+begin
+    process(clk, rst) is
+    begin
+        if (rst = '0') then
+            current_val <= std_logic_vector(to_unsigned(0, current_val'length)); -- natural
+            o_cnt <= '0';
+        elsif (clk'event and clk = '1') then
+            current_val <= std_logic_vector(unsigned(current_val) + 1);
+            if (current_val = preload) then
+                o_cnt <= '1';
+                current_val <= std_logic_vector(to_unsigned(0, current_val'length));
+            else
+                o_cnt <= '0';
+            end if;
+        end if;
+    end process;
+end;
---
+++ VIVADO/PUF_24L_I2C.srcs/sources_1/imports/RTL/scl_gen.vhd
@@ -0,0 +1,113 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use IEEE.numeric_std.all; 
+
+entity scl_gen is
+
+port(
+    fpga_clk: in std_logic;
+    rst: in std_logic;
+    gen_start: in std_logic;
+    gen_stop: in std_logic;
+    o_scl: out std_logic
+);
+
+end entity;
+
+architecture rtl of scl_gen is
+
+    type T_STATE is (IDLE, START, SCL_LOW_EDGE, SCL_LOW, SCL_HI_EDGE, SCL_HI, STOP_WAIT);
+    signal CURRENT_STATE : T_STATE := IDLE;
+    signal NEXT_STATE: T_STATE;
+    signal rep_start: std_logic;
+    signal counter_reset: std_logic;
+    signal overflow_500, overflow_250: std_logic;
+begin
+    
+    rep_start <= '0';
+    count_500: entity work.generic_counter(rtl)
+        generic map(counter_width => 16)
+        port map(
+            clk => fpga_clk,
+            rst => counter_reset,
+            preload => std_logic_vector(to_unsigned(500, 16)),
+            o_cnt => overflow_500
+        );
+
+    count_250: entity work.generic_counter(rtl)
+        generic map(counter_width => 16)
+        port map(
+            clk => fpga_clk,
+            rst => counter_reset,
+            preload => std_logic_vector(to_unsigned(250, 16)),
+            o_cnt => overflow_250
+        );
+    process(fpga_clk, rst, gen_start, gen_stop, CURRENT_STATE, overflow_500, overflow_250, rep_start) is
+    begin
+        counter_reset <= '1';
+        case CURRENT_STATE is
+            when IDLE =>
+                if(rst = '0') then
+                    NEXT_STATE <= IDLE;
+                    counter_reset <= '0';
+                elsif (gen_start = '1') then
+                    --clk_cnt <= 0;
+                    counter_reset <= '1';
+                    NEXT_STATE <= START;
+                else
+                    counter_reset <= '0';
+                    NEXT_STATE <= IDLE;
+                end if;
+
+            when START => --SDA should be pulled low in this state
+                if (overflow_500 = '1') then
+                    NEXT_STATE <= SCL_LOW_EDGE;
+                else
+                    NEXT_STATE <= START;
+                end if;
+
+            when SCL_LOW_EDGE =>
+                o_scl <= '0';
+                --clk_cnt <= 0;
+                NEXT_STATE <= SCL_LOW;
+
+            when SCL_LOW =>
+                if(overflow_500 = '1') then
+                    NEXT_STATE <= SCL_HI_EDGE;
+                else 
+                    NEXT_STATE <= SCL_LOW;
+                end if;
+
+            when SCL_HI_EDGE =>
+                o_scl <= '1';
+                --clk_cnt <= 0;
+                NEXT_STATE <= SCL_HI;
+
+            when SCL_HI =>
+                if(overflow_500 = '1') then
+                    NEXT_STATE <= SCL_LOW_EDGE;
+                else 
+                    NEXT_STATE <= SCL_HI;
+                end if;
+                if (rep_start = '1' AND overflow_250 = '1') then
+                    NEXT_STATE <= START;
+                end if;
+                if(gen_stop = '1' AND overflow_250 = '1') then
+                    --clk_cnt <= 0;
+                    NEXT_STATE <= STOP_WAIT;
+                end if;
+
+            when STOP_WAIT =>
+                if(overflow_500 = '1') then
+                    NEXT_STATE <= IDLE;
+                end if;
+        end case;
+    end process;
+
+    process(fpga_clk) is
+    begin
+        if (fpga_clk'event and fpga_clk = '1') then
+            CURRENT_STATE <= NEXT_STATE;
+        end if;
+    end process;
+end architecture;

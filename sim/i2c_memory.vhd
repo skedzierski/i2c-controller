3d1c1428748fa718f6b8ebfe17bf4364a49e7a15
Szymon <szymon.kedzierski2.stud@pw.edu.pl>
2024-06-09 22:50:53 +0200

    add receivie fifo and register for temperature data

--- sim/i2c_memory.vhd
+++ sim/i2c_memory.vhd
@@ -5,40 +5,35 @@ entity i2c_memory is
     port(
         clk: in std_logic;
         rst: in std_logic;                                            
---        scl: in std_logic;
---        sda: inout std_logic
-        i2c_data : inout std_logic_vector (1 downto 0) 
+        scl_o : out std_logic;
+        sda_o : out std_logic;
+        scl_i : in std_logic;
+        sda_i : in std_logic
     );
 end entity;
 
 architecture sim of i2c_memory is
 
-    signal s_shift_enable_write, send_done, s_shift_enable_read, read_done, sda_i, sda_o  : std_logic;
+    signal s_shift_enable_write, send_done, s_shift_enable_read, read_done : std_logic;
     signal s_data_to_write, s_data_to_read : std_logic_vector(7 downto 0);
-    signal shifted_scl, s_oe : std_logic;
-    signal sda, scl : std_logic;
+    signal shifted_scl, s_oe, s_sda, tx_sda : std_logic;
 
     procedure send_ack(signal sda: out std_logic) is
     begin
         sda <= '0';
-        wait until rising_edge(scl);
-        sda <= 'Z';
-        wait until falling_edge(scl);
+        wait until rising_edge(scl_i);
+        sda <= '1';
+        wait until falling_edge(scl_i);
     end procedure;
 
 begin
-    
-    i2c_data(1) <= 'H';
-    i2c_data(0) <= 'H';
-    scl <= i2c_data(0);
-    sda <= i2c_data(1);
 
     process is 
     begin
-        wait until falling_edge(scl);
+        wait until falling_edge(scl_i);
         wait for 2.5 us;
         shifted_scl <= '1';
-        wait until rising_edge(scl);
+        wait until rising_edge(scl_i);
         wait for 2.5 us;
         shifted_scl <= '0';
     end process;
@@ -51,7 +46,7 @@ begin
         rst => rst,
         shift_enable => s_shift_enable_read,
         parallel_data => s_data_to_read,
-        serial_data => sda,
+        serial_data => sda_i,
         irq => read_done
     );
 
@@ -66,42 +61,44 @@ begin
         shift_enable => s_shift_enable_write,
         oe => s_oe,
         parallel_data => s_data_to_write,
-        serial_data => sda,
+        serial_data => tx_sda,
         irq => send_done
     );
+    sda_o <= tx_sda when s_oe = '1' else s_sda;
     
     process is begin
-        sda <= 'Z';
+        s_sda <= '1';
         s_oe <= '0';
         s_shift_enable_write <= '0';
         s_shift_enable_read <= '0';
-        --sda <= sda_i;
     --wait for start
-        wait until scl = '1' and falling_edge(sda);
+        wait for 100 ns;
+        wait until scl_i = '1' and sda_i = '0';
     --read address
         s_shift_enable_read <= '1';
         wait until read_done = '1';
         s_shift_enable_read <= '0';
     --send ack
         wait for 5 us;
-        send_ack(sda);
-        wait for 5 us;
+        send_ack(s_sda);
     --read pointer
         s_shift_enable_read <= '1';
         wait until read_done = '1';
         s_shift_enable_read <= '0';
     --send ack
-        send_ack(sda);
+        wait for 5 us;
+        send_ack(s_sda);
 --Send:
     --wait for start
-        wait until scl = '1' and falling_edge(sda);
+        wait until scl_i = '1' and falling_edge(sda_i);
         s_data_to_write <= X"BE";
     --read address
         s_shift_enable_read <= '1';
         wait until read_done = '1';
         s_shift_enable_read <= '0';
     --send ack
-        send_ack(sda);
+        wait for 5 us;
+        send_ack(s_sda);
     --send msb data
         s_oe <= '1';
         wait for 10 us;
@@ -112,10 +109,10 @@ begin
         s_oe <= '0';
         s_shift_enable_write <= '0';
         wait for 2.5 us;
-        sda <= '0';
+        s_sda <= '0';
         s_data_to_write <= X"EF";
         wait for 7.5 us;
-        sda <= 'Z';
+        s_sda <= '1';
     --send lsb data
         s_oe <= '1';
         wait for 10 us;
@@ -124,8 +121,8 @@ begin
     --wait for nack
         s_oe <= '0';
         s_shift_enable_write <= '0';
-        wait until falling_edge(scl);
-        wait until rising_edge(scl);
+        wait until falling_edge(scl_i);
+        wait until rising_edge(scl_i);
     --goto send
 
     end process;
--- sim/i2c_top.vhd
+++ sim/i2c_top.vhd
@@ -5,20 +5,19 @@ entity top is
 end entity;
 
 architecture sim of top is
-    signal s_rst, s_clk, s_sda, s_scl, s_btn, s_clk_master : std_logic;
-    signal s_i2c_data : std_logic_vector (1 downto 0);
+    signal s_rst, s_clk, s_btn, s_clk_master : std_logic;
+    signal s_scl_o, s_scl_i, s_sda_o, s_sda_i : std_logic;
 begin
 
     --s_sda <= 'H';
     --s_scl <= 'H';
 
-    s_i2c_data(0) <= 'H';
-    s_i2c_data(1) <= 'H'; 
     master: entity work.i2c(rtl)
     port map(
-        --scl => s_scl,
-        --sda => s_sda,
-        i2c_data => s_i2c_data,
+        scl_o => s_scl_o,
+        sda_o => s_sda_o,
+        scl_i => s_scl_i,
+        sda_i => s_sda_i,
         clk => s_clk,
         btn => s_btn,
         rst => s_rst
@@ -28,23 +27,17 @@ begin
      port map(
          rst => s_rst,
          clk => s_clk,                                 
-         --scl => s_scl,
-         --sda => s_sda
-         i2c_data => s_i2c_data
+         scl_o => s_scl_i,
+         sda_o => s_sda_i,
+         scl_i => s_scl_o,
+         sda_i => s_sda_o
      );
 
     clk_gen : process is begin
         s_clk <= '1';
-        wait for 83.33 ns;
+        wait for 5 ns;
         s_clk <= '0';
-        wait for 83.33 ns;
-    end process;
-    
-    clk_gen_master : process is begin
-        s_clk_master <= '1';
-        wait for 83.33 ns;
-        s_clk_master <= '0';
-        wait for 83.33 ns;
+        wait for 5 ns;
     end process;
 
 
